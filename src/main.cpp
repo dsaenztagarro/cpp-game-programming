a873f7baaf3fa48e7065e9c8cf7b89b116af131a
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-06-26 07:49:50 +0200

    WIP playing with file system events

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
-using namespace std;
+#include <CoreServices/CoreServices.h>
+
+using std::cout;
+using std::endl;
 
 int main()
 {
-  cout << "Game Over!" << endl;
-  return 0;
+  /* Define variables and create a CFArray object containing
+     CFString objects containing paths to watch.
+   */
+  CFStringRef mypath = CFSTR("/tmp/test");
+  CFArrayRef pathsToWatch = CFArrayCreate(NULL, (const void **)&mypath, 1, NULL);
+  void *callbackInfo = NULL; // could put stream-specific data here.
+  FSEventStreamRef stream;
+  CFAbsoluteTime latency = 3.0; /* Latency in seconds */
+
+  /* Create the stream, passing in a callback */
+  stream = FSEventStreamCreate(NULL,
+      &myCallbackFunction,
+      callbackInfo,
+      pathsToWatch,
+      kFSEventStreamEventIdSinceNow, /* Or a previous event ID */
+      latency,
+      kFSEventStreamCreateFlagNone /* Flags explained in reference */
+  );
 }
